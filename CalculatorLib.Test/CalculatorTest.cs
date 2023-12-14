fc36b031632bc97b19c9dbf8a87aad96d7ada283
HakanAk <hakanak54@gmail.com>
2023-12-14 18:19:38 +0300

    Add project files.

---
+++ CalculatorLib.Test/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib.Test
+{
+
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(7, 10);
+            Assert.Equal(17, result);
+
+
+        }
+
+    }
+}
